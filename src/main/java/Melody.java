fba4d1e8732f5ffe1884bfcb1bfdc443ba70c778
feliciaz05 <felicia.sgp@gmail.com>
2025-08-16 23:30:21 +0800

    Add Melody.java

---
+++ src/main/java/Melody.java
@@ -0,0 +1,23 @@
+/*public class Duke {
+    public static void main(String[] args) {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
+    }
+}*/
+
+public class Melody {
+    public static void main(String[] args) {
+        String logo = " __  __      _           _       \n"
+                + "|  \\/  | ___| | ___   __| |_   _ \n"
+                + "| |\\/| |/ _ \\ |/ _ \\ / _` | | | |\n"
+                + "| |  | |  __/ | (_) | (_| | |_| |\n"
+                + "|_|  |_|\\___|_|\\___/ \\__,_|\\__, |\n"
+                + "                          |___/ \n";
+        String exitLine = "Toodles! See you next time~\n";
+        System.out.println("Hello from\n" + logo + "\n" + exitLine);
+    }
+}
