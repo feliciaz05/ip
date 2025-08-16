f415a46e9085c2def4bce71038a3b0ccb7fc073d
feliciaz05 <felicia.sgp@gmail.com>
2025-08-17 00:27:33 +0800

    Add Melody.java

--- src/main/java/Melody.java
+++ src/main/java/Melody.java
@@ -9,6 +9,8 @@
     }
 }*/
 
+import java.util.Scanner;
+
 public class Melody {
 
     public static void main(String[] args) {
@@ -18,7 +20,26 @@ public class Melody {
                 + "| |  | |  __/ | (_) | (_| | |_| |\n"
                 + "|_|  |_|\\___|_|\\___/ \\__,_|\\__, |\n"
                 + "                          |___/ \n";
-        String exitLine = "Toodles! See you next time~\n";
-        System.out.println("Hello from\n" + logo + "\n" + exitLine);
+        String exitLine = "Toodles! See you next time~";
+        System.out.println("Hello! I'm\n" + logo + "\n" + "What can I do for you?\n" + " ______");
+
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        while (true) {
+            input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                System.out.println("  " + exitLine);
+                System.out.println("______");
+                break;
+            }
+
+            System.out.println("  " + input);
+            System.out.println("______");
+        }
+
+        scanner.close();
+
     }
 }
