4e0a1f245dbdb37d2348d29d624468f3cdda605e
feliciaz05 <felicia.sgp@gmail.com>
2025-08-17 19:50:54 +0800

    Add Melody.java

--- src/main/java/Melody.java
+++ src/main/java/Melody.java
@@ -15,7 +15,7 @@ import java.util.Scanner;
 
 public class Melody {
 
-    private static ArrayList<String> tasks = new ArrayList<>();
+    private static ArrayList<Task> tasks = new ArrayList<>();
 
     public static void main(String[] args) {
         String logo = " __  __      _           _       \n"
@@ -39,6 +39,12 @@ public class Melody {
                 break;
             } else if (input.equals("list")) {
                 listTasks();
+            } else if (input.contains("unmark ")) {
+                int taskNumber = Integer.parseInt(input.substring(7).trim());
+                markTask(taskNumber, false);
+            } else if (input.contains("mark ")) {
+                int taskNumber = Integer.parseInt(input.substring(5).trim());
+                markTask(taskNumber, true); //whatever is at position 5 is the task no.
             } else {
                 addTask(input);
             }
@@ -49,16 +55,44 @@ public class Melody {
 
     }
 
-    private static void addTask(String task) {
-        tasks.add(task);
-        System.out.println("  added: " + task);
+    private static void addTask(String desc) {
+        Task newTask = new Task(desc);
+        tasks.add(newTask);
+        System.out.println("  added: " + desc);
         System.out.println("______");
     }
 
     private static void listTasks() {
+        System.out.println("Here are the tasks in your list: \n");
         for (int i = 0; i < tasks.size(); i++) {
-            System.out.println((i + 1) + ". " + tasks.get(i));
+            Task task = tasks.get(i);
+            System.out.println((i + 1) + ". [" + task.getStatusIcon() + "] " + task.description);
         }
         System.out.println("______");
     }
+
+    private static void markTask(int taskNumber, boolean isDone) {
+        System.out.println(isDone ? "  Okie! I've marked it as completed!" : "  Alright! It's unmarked!");
+        Task task = tasks.get(taskNumber - 1);
+        task.isDone = isDone;
+        System.out.println("    [" + task.getStatusIcon() + "] " + task.description);
+        System.out.println("______");
+    }
+
+    public static class Task {
+        protected String description;
+        protected boolean isDone;
+
+        public Task(String description) {
+            this.description = description;
+            this.isDone = false;
+        }
+
+        public String getStatusIcon() {
+            return (isDone ? "X" : " "); // mark done task with X
+        }
+
+            //...
+    }
+
 }
