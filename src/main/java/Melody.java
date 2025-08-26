f788b0204ee5a56b1e0b238850a026147be6e4c5
feliciaz05 <felicia.sgp@gmail.com>
2025-08-26 23:21:55 +0800

    Extract core components into OOP structure

--- src/main/java/Melody.java
+++ src/main/java/Melody.java
@@ -49,106 +49,51 @@ public class Melody {
     }
 
     private static void handleCommand(String input) throws MelodyException {
-        if (input.equals("bye")) {
-            ui.showGoodbye();
-            ui.close();
-            System.exit(0);
-        } else if (input.equals("list")) {
-            ui.showTaskList(tasks.getTasksAsString());
-        } else if (input.startsWith("unmark ")) {
-            handleMarkCommand(input, false);
-        } else if (input.startsWith("mark ")) {
-            handleMarkCommand(input, true);
-        } else if (input.startsWith("deadline ")) {
-            handleDeadline(input);
-        } else if (input.startsWith("todo ")) {
-            handleTodo(input);
-        } else if (input.startsWith("event ")) {
-            handleEvent(input);
-        } else if (input.startsWith("delete ")) {
-            handleDeleteCommand(input);
-        } else {
-            throw new MelodyException("I don't understand that command. Try: todo, deadline, event, list, mark, unmark, delete, or bye!");
+        CommandType commandType = Parser.parseCommand(input);
+
+        switch (commandType) {
+            case BYE:
+                ui.showGoodbye();
+                ui.close();
+                System.exit(0);
+                break;
+
+            case LIST:
+                ui.showTaskList(tasks.getTasksAsString());
+                break;
+
+            case MARK:
+                int markTaskNumber = Parser.parseTaskNumber(input);
+                markTask(markTaskNumber, true);
+                break;
+
+            case UNMARK:
+                int unmarkTaskNumber = Parser.parseTaskNumber(input);
+                markTask(unmarkTaskNumber, false);
+                break;
+
+            case TODO:
+                String todoDesc = Parser.parseTodo(input);
+                addTodo(todoDesc);
+                break;
+
+            case DEADLINE:
+                String[] deadlineParts = Parser.parseDeadline(input);
+                addDeadline(deadlineParts[0], deadlineParts[1]);
+                break;
+
+            case EVENT:
+                String[] eventParts = Parser.parseEvent(input);
+                addEvent(eventParts[0], eventParts[1], eventParts[2]);
+                break;
+
+            case DELETE:
+                int deleteTaskNumber = Parser.parseTaskNumber(input);
+                Task removedTask = tasks.removeTask(deleteTaskNumber);
+                ui.showTaskRemoved(removedTask, tasks.size());
+                saveTasksToFile();
+                break;
         }
-    }
-
-    private static void handleMarkCommand(String input, boolean isDone) throws MelodyException {
-        try {
-            String numberStr = input.split(" ")[1];
-            int taskNumber = Integer.parseInt(numberStr);
-            markTask(taskNumber, isDone);
-        } catch (Exception e) {
-            throw new MelodyException("Please enter a valid task number after '" + (isDone ? "mark" : "unmark") + "'");
-        }
-    }
-
-    private static void handleDeadline(String input) throws MelodyException {
-        try {
-            if (input.equals("deadline")) {
-                throw new MelodyException("A deadline needs both a description and time. Try: 'deadline <task> /by <time>'");
-            }
-            int byIndex = input.indexOf(" /by ");
-            if (byIndex == -1) {
-                throw new MelodyException("Missing '/by' in deadline. Try: 'deadline <task> /by <time>'");
-            }
-            String ddl = input.substring(byIndex + 5).trim();
-            String desc = input.substring(9, byIndex).trim();
-            if (desc.isEmpty()) {
-                throw new MelodyException("The description of a deadline cannot be empty.f");
-            }
-            addDeadline(desc, ddl);
-        } catch (StringIndexOutOfBoundsException e) {
-            throw new MelodyException("Invalid deadline format. Try: 'deadline <task> /by <time>'");
-        }
-    }
-
-    private static void handleTodo(String input) throws MelodyException {
-        String desc = input.substring(5).trim();
-        if (desc.isEmpty()) {
-            throw new MelodyException("The description of a todo cannot be empty. Try: 'todo <task>'");
-        }
-        addTodo(desc);
-    }
-
-    private static void handleEvent(String input) throws MelodyException {
-        try {
-            if (input.equals("event")) {
-                throw new MelodyException("An event needs description, start and end times. Try: 'event <task> /from <start> /to <end>'");
-            }
-            int fromIndex = input.indexOf(" /from ");
-            int toIndex = input.indexOf(" /to ");
-            if (fromIndex == -1 || toIndex == -1) {
-                throw new MelodyException("Missing '/from' or '/to' in event. Try: 'event <task> /from <start> /to <end>'");
-            }
-            String fromTime = input.substring(fromIndex + 7, toIndex).trim();
-            String toTime = input.substring(toIndex + 5).trim();
-            String desc = input.substring(6, fromIndex).trim();
-            if (desc.isEmpty()) {
-                throw new MelodyException("The description of an event cannot be empty");
-            }
-            addEvent(desc, fromTime, toTime);
-        } catch (StringIndexOutOfBoundsException e) {
-            throw new MelodyException("Invalid event format. Try: 'event <task> /from <start> /to <end>'");
-        }
-    }
-
-    private static void handleDeleteCommand(String input) throws MelodyException {
-        try {
-            String numberStr = input.split(" ")[1];
-            int taskNumber = Integer.parseInt(numberStr);
-
-            Task removedTask = tasks.removeTask(taskNumber);
-            ui.showTaskRemoved(removedTask, tasks.size());
-            saveTasksToFile();
-        } catch (ArrayIndexOutOfBoundsException e) {
-            throw new MelodyException("Please specify a task number to delete.");
-        } catch (NumberFormatException e) {
-            throw new MelodyException("Please enter a valid task number.");
-        }
-    }
-
-    private static void listTasks() {
-        System.out.println(tasks.getTasksAsString());
     }
 
     private static void markTask(int taskNumber, boolean isDone) throws MelodyException {
